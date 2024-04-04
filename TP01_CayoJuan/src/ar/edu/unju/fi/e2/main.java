23fe2d31cc1bc63641a7736776dc1d958f30cc68
PC <PC@DESKTOP-75A9JNB>
2024-04-04 15:01:39 -0300

    Paquete clase 1

---
+++ TP01_CayoJuan/src/ar/edu/unju/fi/e2/main.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.e2;
+
+public class main {
+	public static void main(String[] args) {
+		 String nombrePais = "Argentina";
+	        int edadPersona = 30;
+	        double alturaEdificio = 100.5; // Altura en metros
+	        double precioProducto = 10.99; // Precio en la moneda local
+	        String numeroTelefono = "123456789";
+	        double coseno = Math.cos(0.5);
+	        System.out.println("Nombre del país: " + nombrePais + ", Edad de la persona: " + edadPersona + ", Altura del edificio: " + alturaEdificio + " metros, Precio del producto: $" + precioProducto + ", Número de teléfono: " + numeroTelefono + ", Cálculo del coseno de 0.5: " + coseno);
+
+	
+	
+	}
+}
