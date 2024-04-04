d0cf8f6a3c382b143a78e88e89ff2a3bd39b1f9b
PC <PC@DESKTOP-75A9JNB>
2024-04-04 17:07:29 -0300

    Ejercicios 2 y 4

--- TP01_CayoJuan/src/ar/edu/unju/fi/e2/main.java
+++ TP01_CayoJuan/src/ar/edu/unju/fi/e2/main.java
@@ -4,8 +4,8 @@ public class main {
 	public static void main(String[] args) {
 		 String nombrePais = "Argentina";
 	        int edadPersona = 30;
-	        double alturaEdificio = 100.5; // Altura en metros
-	        double precioProducto = 10.99; // Precio en la moneda local
+	        double alturaEdificio = 100.5;
+	        double precioProducto = 10.99;
 	        String numeroTelefono = "123456789";
 	        double coseno = Math.cos(0.5);
 	        System.out.println("Nombre del país: " + nombrePais + ", Edad de la persona: " + edadPersona + ", Altura del edificio: " + alturaEdificio + " metros, Precio del producto: $" + precioProducto + ", Número de teléfono: " + numeroTelefono + ", Cálculo del coseno de 0.5: " + coseno);
