109e13472c7f5f970784e9f0fc159e53710d3804
PC <PC@DESKTOP-75A9JNB>
2024-04-11 01:05:55 -0300

    .

--- TP01_CayoJuan/src/ar/edu/unju/fi/e4/main.java
+++ TP01_CayoJuan/src/ar/edu/unju/fi/e4/main.java
@@ -1,69 +1,32 @@
 package ar.edu.unju.fi.e4;
-
- 
-
 import java.util.Scanner;
-
- 
-
 public class main {
 
- 
-
 	public static void main(String[] args) {
-
 		// TODO Auto-generated method stub
-
-		Scanner scanner = new Scanner(System.in);
-
-		
-
+		Scanner scanner = new Scanner(System.in);		
 		long factorial = 1;
-
-		System.out.println("Ingrese numero :");
-
-		
-
-		
-
+		System.out.println("Ingrese numero :");				
 		if(scanner.hasNextInt()) {
-
 			int num = scanner.nextInt();
-
 			if(num<0 || num >10) {
-
 				System.out.println("Solo calcula con numeros del 0 al 10 !");
-
 			}
-
 			else {
-
 				while(num!=0) {
-
 					factorial=factorial*num;
-
 					num--;
-
 				}
-
 				System.out.println("El FACTORIAL ES: "+factorial);
-
 				scanner.close();
-
 			}
-
 		}
 
 		else {
 
 			System.out.println("Se ingreso datos incorrectos");
 
-		}
-
-		
-
-		
-
+		}		
 	}
 
 }
